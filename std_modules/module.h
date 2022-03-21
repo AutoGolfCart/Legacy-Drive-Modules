4818dbc3a35686678ee5d8ab7dc95a92f91c8167
The1TrueJoe <jttelaak@hotmail.com>
2022-03-21 15:38:40 -0400

    std can messages

--- std_modules/module.h
+++ std_modules/module.h
@@ -2,6 +2,10 @@
 #include "can_adapter.h"
 #include "id_light.h"
 
+#define COM_SET 0x0A
+#define COM_OP 0x0B
+#define COM_GET 0x0C
+
 public:
     void standardModuleSetup(int CAN_CS_PIN = Default_CAN_CS);
     void standardModuleLoopHead();
